d237538768a7c43825976a72de0b299b4f5f51e2
Juan Gerardo <jmendez1025@cue.edu.co>
2025-08-03 18:46:02 -0500

    feat(user): add logging to CreateUserUseCaseImpl for user creation flow

--- src/main/java/com/gerard/barbershop/userservice/application/usecases/impl/CreateUserUseCaseImpl.java
+++ src/main/java/com/gerard/barbershop/userservice/application/usecases/impl/CreateUserUseCaseImpl.java
@@ -7,11 +7,13 @@ import com.gerard.barbershop.userservice.domain.exceptions.UserEmailAlreadyExist
 import com.gerard.barbershop.userservice.domain.models.User;
 import com.gerard.barbershop.userservice.domain.ports.out.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CreateUserUseCaseImpl implements CreateUserUseCase {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
@@ -19,8 +21,13 @@ public class CreateUserUseCaseImpl implements CreateUserUseCase {
     @Override
     public Mono<User> execute(CreateUserCommand createUserCommand) {
         return validateEmailAvailable(createUserCommand.email())
-                .map(ignored -> userMapper.toDomain(createUserCommand))
-                .flatMap(userRepository::saveUser);
+                .then(Mono.fromCallable(() -> userMapper.toDomain(createUserCommand)))
+                .flatMap(user -> {
+                    log.info("Saving user to DB: {}", user.getEmail());
+                    return userRepository.saveUser(user);
+                })
+                .doOnSuccess(user -> log.info("User saved: {}", user))
+                .doOnError(error -> log.error("Error during create user", error));
     }
 
     private Mono<Void> validateEmailAvailable(String email) {
