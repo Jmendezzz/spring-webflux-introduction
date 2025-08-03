b4e604891a740852ebc35029adebc1b62fa13382
Juan Gerardo <jmendez1025@cue.edu.co>
2025-08-03 18:46:11 -0500

    refactor(entity): explicitly map UserEntity fields with @Column

--- src/main/java/com/gerard/barbershop/userservice/infrastructure/persistence/entities/UserEntity.java
+++ src/main/java/com/gerard/barbershop/userservice/infrastructure/persistence/entities/UserEntity.java
@@ -2,13 +2,19 @@ package com.gerard.barbershop.userservice.infrastructure.persistence.entities;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @Table("users")
 public class UserEntity {
     @Id
+    @Column("id")
     private Long id;
+
+    @Column("email")
     private String email;
+
+    @Column("password")
     private String password;
 }
